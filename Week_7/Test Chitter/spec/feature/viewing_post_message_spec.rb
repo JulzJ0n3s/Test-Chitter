d1ef97ab95667dd8bdb9b4f6e8641cbbbf4c46e5
Julia Jones <j.jones.13@ucl.ac.uk>
2020-01-21 11:09:31 +0000

    Syntax error with the PG connect

--- Week_7/Test Chitter/spec/feature/viewing_post_message_spec.rb
+++ Week_7/Test Chitter/spec/feature/viewing_post_message_spec.rb
@@ -3,19 +3,10 @@ require 'pg'
 feature 'Posting peeps on chitter' do
     scenario 'user message post on chitter' do
         connection = PG.connect(dbname: 'chitter_manager')
-        result = connection.exec('SELECT * FROM chitter_peeps')
-        result.map { |bookmark| bookmark['text'] }
 
         visit ('/peeps')
-        first('.peeps').click_button 'Add Comment'
-
-        expect(current_path).to eq "/peeps/#{peep.id}/new"
-
-        fill_in 'comment', with: 'A peep a day...'
-        click_button 'Submit'
         
-        expect(current_path).to eq '/peeps'
-        expect(first('.peeps')).to have_content 'this is a test peep'
+        expect(page).to have_content 'This is a test peep'
 
     end
 end
--- Week_7/Test Chitter/spec/v_peeps_spec.rb
+++ Week_7/Test Chitter/spec/v_peeps_spec.rb
@@ -3,14 +3,14 @@ require 'peeps'
 describe Peeps do 
     describe '.all' do
         it 'returns a list of peeps' do
-        connection = PG.connect(dbname: 'chitter_manager_test')
+        connection = PG.connect(dbname: 'chitter_manager')
     
         # Add the test data
-        connection.exec("INSERT INTO chitter_peeps ('''') VALUES ('''');")
+        connection.exec("INSERT INTO chitter_peeps (title, peep, time_id) VALUES ('Where have you been?', 'Hello Good people, Welcome to database week, I'm your Chitter bot! This is a test peep', NOW()");
     
-        bookmarks = Bookmark.all
+        peeeps = peep.all
     
-        expect(bookmarks).to include('''')
+        expect(peeeps).to include('Where have you been?')
         
         end
     end
